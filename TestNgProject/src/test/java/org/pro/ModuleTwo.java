cb69d751b610963586970935ac59d215a7fc5805
nareshhardcoded <nareshrespons@gmail.com>
2025-02-27 17:42:20 +0530

    second screen shot

--- TestNgProject/src/test/java/org/pro/ModuleTwo.java
+++ TestNgProject/src/test/java/org/pro/ModuleTwo.java
@@ -11,7 +11,7 @@ import org.testng.annotations.Test;
 
 public class ModuleTwo extends BaseClass {
 
-	@Test()
+	@Test(enabled = false)
 	private void tc1() throws InterruptedException {
 		launchBrowser();
 		launchUrl("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
@@ -24,17 +24,18 @@ public class ModuleTwo extends BaseClass {
 
 	}
 
-	@Test(priority = -5)
+	@Test(priority = 10)
 	// takescreenshot
 	private void tc2() throws IOException, InterruptedException {
 		System.out.println("Test Case 2");
+		launchBrowser();
 		launchUrl("https://profile.w3schools.com/login");
 		windowMaximize();
 		Thread.sleep(5000);
-		SimpleDateFormat dateFormat = new SimpleDateFormat("dd-mm-yyyy//h-m-s");
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd-m-yyyy//h-m-s");
 		Date date = new Date();
 		takeScreenShot(
-				"C:\\Users\\LENOVO\\eclipse-workspace\\TestNgProject\\screenshots\\scrn-"+dateFormat.format(date)+".png");
+				"C:\\Users\\LENOVO\\git\\seleniumproject\\TestNgProject\\screenshots\\"+dateFormat.format(date)+".png");
 
 	}
 
@@ -46,7 +47,7 @@ public class ModuleTwo extends BaseClass {
 		windowMaximize();
 	}
 	
-	@Test(priority = -10)
+	@Test()
 	private void tc4() {
 		System.out.println("Test Case 4");
 		launchBrowser();
@@ -59,7 +60,7 @@ public class ModuleTwo extends BaseClass {
 
 	}
 	
-	@Test(priority = -15)
+	@Test(priority = -5)
 	private void tc5() throws IOException {
 		System.out.println("Test Case 5");
 		excelReadNew("C:\\Users\\LENOVO\\eclipse-workspace\\TestNgProject\\excel\\SampleData.xlsx", "Data");
